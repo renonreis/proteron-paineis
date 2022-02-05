0d29a3dc71f12fe8267531a0048fcdec4171f34f
aurudin <renon@renon.com.br>
2022-02-05 11:18:40 -0300

    feat: add .

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,46 +9,29 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
 
-	<?php proteron_paineis_post_thumbnail(); ?>
+				<?php proteron_paineis_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+				<div class="entry-content">
+					<?php
+					the_content();
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'proteron-paineis' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'proteron-paineis' ),
+					wp_link_pages(
 						array(
-							'span' => array(
-								'class' => array(),
-							),
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'proteron-paineis' ),
+							'after'  => '</div>',
 						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+					);
+					?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-<?php the_ID(); ?> -->
+		</div>
+	</div>
+</div>
